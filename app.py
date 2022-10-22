b7f6fa1ef5eedc9445d9e09d16cf711f678b9f7a
Vikreth Oun <96528416+vikreth@users.noreply.github.com>
2022-10-22 15:04:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
 import streamlit as st
-import numpy as np
-import string
-import pickle
-st.set_option('deprecation.showfileUploaderEncoding', False)
-model = pickle.load(open('model_pkl.pkl', 'rb'))
+import joblib
+import pandas as pd
+from PIL import Image
 
+@st.cache(allow_output_mutation=True)
+def load(scaler_path, model_path):
+    sc = joblib.load(scaler_path)
+    model = joblib.load(model_path)
+    return sc , model
 
-def main():
-    st.header("ការប៉ាន់ស្មាននៃការកើតជម្ងឺទឹកនោមផ្អែមចំពោះស្ត្រី")
-    st.subheader("សូមបញ្ចូលព័ត៌មានរបស់អ្នកដូចខាងក្រោម")
+def inference(row, scaler, model, feat_cols):
+    df = pd.DataFrame([row], columns = feat_cols)
+    X = scaler.transform(df)
+    features = pd.DataFrame(X, columns = feat_cols)
+    if (model.predict(features)==0):
+        return "This is a healthy person!"
+    else: return "This person has high chances of having diabetics!"
 
-    Pregnancies = st.slider("Input Your Number of Pregnancies", 0, 16)
-    Glucose = st.slider("Input your Gluclose", 74, 200)
-    BloodPressure = st.slider("Input your Blood Pressure", 30, 130)
-    SkinThickness = st.slider("Input your Skin thickness", 0, 100)
-    Insulin = st.slider("Input your Insulin", 0, 200)
-    BMI = st.slider("Input your BMI", 14.0, 60.0)
-    DiabetesPedigreeFunction = st.slider(
-        "Input your Diabetes Pedigree Function", 0.0, 6.0)
-    Age = st.slider("Input your Age", 0, 100)
+st.title('Diabetes Prediction App')
+st.write('The data for the following example is originally from the National Institute of Diabetes and Digestive and Kidney Diseases and contains information on females at least 21 years old of Pima Indian heritage. This is a sample application and cannot be used as a substitute for real medical advice.')
+image = Image.open('data/diabetes_image.jpg')
+st.image(image, use_column_width=True)
+st.write('Please fill in the details of the person under consideration in the left sidebar and click on the button below!')
 
-    inputs = [[Pregnancies, Glucose, BloodPressure, SkinThickness,
-               Insulin, BMI, DiabetesPedigreeFunction, Age]]
+age =           st.sidebar.number_input("Age in Years", 1, 150, 25, 1)
+pregnancies =   st.sidebar.number_input("Number of Pregnancies", 0, 20, 0, 1)
+glucose =       st.sidebar.slider("Glucose Level", 0, 200, 25, 1)
+skinthickness = st.sidebar.slider("Skin Thickness", 0, 99, 20, 1)
+bloodpressure = st.sidebar.slider('Blood Pressure', 0, 122, 69, 1)
+insulin =       st.sidebar.slider("Insulin", 0, 846, 79, 1)
+bmi =           st.sidebar.slider("BMI", 0.0, 67.1, 31.4, 0.1)
+dpf =           st.sidebar.slider("Diabetics Pedigree Function", 0.000, 2.420, 0.471, 0.001)
 
-    if st.button('Predict'):
-        result = model.predict(inputs)
-        updated_res = result.flatten().astype(int)
-        if updated_res == 0:
-            st.write("អ្នកមិនមានជម្ងឺទឹកនោមផ្អែមទេ។")
-        else:
-            st.write(
-                "អ្នកប្រហែលជាបានកើតជម្ងឺទឹកនោមផ្អែមហើយ​ សូមប្រញាប់ទៅជួបគ្រូពេទ្យដែលនៅជិតអ្នកបំផុត។")
+row = [pregnancies, glucose, bloodpressure, skinthickness, insulin, bmi, dpf, age]
 
+if (st.button('Find Health Status')):
+    feat_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
 
-if __name__ == '__main__':
-    main()
+    sc, model = load('models/scaler.joblib', 'models/model.joblib')
+    result = inference(row, sc, model, feat_cols)
+    st.write(result)
